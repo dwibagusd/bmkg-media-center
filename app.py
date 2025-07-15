d8adce099c8a6f105025a1ccb5812853c7af7d6f
dwibagusd <115286631+dwibagusd@users.noreply.github.com>
2025-07-15 11:59:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -357,5 +357,5 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
     if not all([os.getenv('SUPABASE_URL'), os.getenv('SUPABASE_KEY')]):
-    raise RuntimeError("Supabase credentials missing!")
+        raise RuntimeError("Supabase credentials missing!")
     
